adf86f13cf24b4a22966a000a54a2f46077b1282
esruthegreat <esrugamer@gmail.com>
2016-08-19 22:44:16 -0500

    Update EY_Website_Script.js

--- EY_Website_Script.js
+++ EY_Website_Script.js
@@ -1,1 +1,13 @@
-/*$(document).ready();*/
+$(document).ready(jQuery.fn.htmlClean = function() {
+    this.contents().filter(function() {
+        if (this.nodeType != 3) {
+            $(this).htmlClean();
+            return false;
+        }
+        else {
+            this.textContent = $.trim(this.textContent);
+            return !/\S/.test(this.nodeValue);
+        }
+    }).remove();
+    return this;
+});
